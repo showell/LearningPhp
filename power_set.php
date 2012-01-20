cbdd0cc1b350a32aab9bb5899b000776050e46c2
Steve Howell <steve@strings.com>
2012-01-20 12:52:16 -0800

    cleanup

--- power_set.php
+++ power_set.php
@@ -1,14 +1,14 @@
 <?php
-function output($binary, $arr) {
+function get_subset($binary, $arr) {
   // based on true/false values in $binary array, print
   // values in $arr
   $subset = array();
   foreach (range(0, count($arr)-1) as $i) {
     if ($binary[$i]) {
-      $subset[] = $arr[$i];
+      $subset[] = $arr[count($arr) - $i - 1];
     } 
   }
-  print_array($subset);
+  return $subset;
 }
 
 function print_array($arr) {
@@ -22,14 +22,21 @@ function print_array($arr) {
 
 function print_power_sets($arr) {
   echo "POWER SET of [" . join(", ", $arr) . "]<br>";
+  foreach (power_set($arr) as $subset) {
+    print_array($subset);
+  }
+}
+  
+function power_set($arr) {  
   $binary = array();
   foreach (range(1, count($arr)) as $i) {
     $binary[] = false;
   }
   $n = count($arr);
+  $powerset = array();
   
   while (count($binary) <= count($arr)) {
-    output($binary, $arr);
+    $powerset[] = get_subset($binary, $arr);
     $i = 0;
     while (true) {
       if ($binary[$i]) {
@@ -42,6 +49,8 @@ function print_power_sets($arr) {
     }
     $binary[$i] = true;
   }
+  
+  return $powerset;
 }
  
 print_power_sets(array());
